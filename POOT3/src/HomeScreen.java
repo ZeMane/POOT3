c2ef7109265c8fda072dff91fd551bdd1eb5994d
ZeMane <rbiffineto@gmail.com>
2015-05-21 13:06:55 -0300

    [teste] - tentando tirar o arquivo de testes

---
+++ POOT3/src/HomeScreen.java
@@ -0,0 +1,16 @@
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeScreen implements javafx.fxml.Initializable {
+	@FXML
+	private Button btnPrintHello;
+
+	@Override	
+	public void initialize(URL location, ResourceBundle resources) {		
+	}	
+}
+
